19c97672e36c16bd9d5f96dda2f11fb1227c0762
Lukas Kubis <me@lukaskubis.net>
2017-10-12 11:19:48 +0200

    namespace fixes, added production settings

--- samples/IdentitySample.Web/Pages/Account/Manage/SetPassword.cshtml.cs
+++ samples/IdentitySample.Web/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using IdentitySample.Web.Data;
 
-namespace IdentitySample.Pages.Account.Manage
+namespace IdentitySample.Web.Pages.Account.Manage
 {
     public class SetPasswordModel : PageModel
     {
